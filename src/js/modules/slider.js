5e423cc8b0539f394e1d776ba5774354bb728353
Vlas1d <vlasid753@gmail.com>
2023-03-05 00:03:48 +0200

    cataloge 1

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -10,4 +10,28 @@ export function sliders() {
             spaceBetween: 33,
         });
     }
+
+    if (document.querySelector('.reviews') != null) {
+        const goods_swiper = new Swiper('.reviews', {
+            modules: [Navigation, Pagination],
+            loop: true,
+            slidesPerView: "auto",
+            spaceBetween: 32,
+        });
+    }
+
+    if (document.querySelector('.cataloge-slider') != null) {
+        const goods_swiper = new Swiper('.cataloge-slider', {
+            modules: [Navigation, Pagination],
+            loop: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            spaceBetween: 50,
+
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    }
 }
